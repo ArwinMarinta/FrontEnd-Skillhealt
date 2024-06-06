897f092388de035de2581fda415ba750faa4de3e
Arwin Marinta <arwinmarinta3@gmail.com>
2024-06-07 02:43:14 +0800

    setup folder

---
+++ src/router/index.ts
@@ -0,0 +1,22 @@
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "../pages/home/Home.vue";
+import Login from "../pages/login/Login.vue";
+// import Register from "@/pages/register/Register.vue";
+
+const routes = [
+  {
+    path: "/",
+    component: Home,
+  },
+  {
+    path: "/login",
+    component: Login,
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: routes,
+});
+
+export default router;
